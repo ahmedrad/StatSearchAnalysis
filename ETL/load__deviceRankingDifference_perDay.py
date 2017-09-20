08ed325c54f31123f907b10d956718fd2bfe369a
Ahmed Radwan <ahmedrad@gmail.com>
2017-09-19 17:24:31 -0700

    Fixing incorrect sorting

--- ETL/load__deviceRankingDifference_perDay.py
+++ ETL/load__deviceRankingDifference_perDay.py
@@ -1,7 +1,7 @@
 from initializer import *
 
 device_ranking_difference = sqlContext.read.parquet(get_out_dir() + '/compute__deviceRankingDifference_perDay.parquet')
-sorted_data = device_ranking_difference.sort(device_ranking_difference.CrawlDate.desc())
+sorted_data = device_ranking_difference.sort(device_ranking_difference.CrawlDate.asc())
 
 out_file = os.path.join(get_out_dir(), 'deviceRankingDifference_perDay.csv')
 sorted_data.repartition(1).write.csv(out_file, mode='overwrite', header=True)
